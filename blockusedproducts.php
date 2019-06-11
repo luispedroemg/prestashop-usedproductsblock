41e99745187af06c990bb4e3a372bcf5e91281bb
The Dev <luispedro.emg@gmail.com>
2019-06-11 22:32:20 +0100

    Cleanup

--- blockusedproducts.php
+++ blockusedproducts.php
@@ -1,29 +1,4 @@
 <?php
-/*
-* 2007-2016 PrestaShop
-*
-* NOTICE OF LICENSE
-*
-* This source file is subject to the Academic Free License (AFL 3.0)
-* that is bundled with this package in the file LICENSE.txt.
-* It is also available through the world-wide-web at this URL:
-* http://opensource.org/licenses/afl-3.0.php
-* If you did not receive a copy of the license and are unable to
-* obtain it through the world-wide-web, please send an email
-* to license@prestashop.com so we can send you a copy immediately.
-*
-* DISCLAIMER
-*
-* Do not edit or add to this file if you wish to upgrade PrestaShop to newer
-* versions in the future. If you wish to customize PrestaShop for your
-* needs please refer to http://www.prestashop.com for more information.
-*
-*  @author PrestaShop SA <contact@prestashop.com>
-*  @copyright  2007-2016 PrestaShop SA
-*  @license    http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
-*  International Registered Trademark & Property of PrestaShop SA
-*/
-
 if (!defined('_PS_VERSION_'))
 	exit;
 
@@ -62,6 +37,7 @@ if (!class_exists('BlockUsedProducts')) {
                 && Configuration::updateValue('PS_USED_P_THRESHOLD_DAYS', 7)
                 && Configuration::updateValue('PS_USED_PRODUCT_SHOW_AS_NEW', 0)
                 && Configuration::updateValue('PS_USED_PRODUCT_SHOW_USED', 1)
+                && Configuration::updateValue('PS_NB_DAYS_USED_PRODUCT', 30)
                 && $this->registerHook('displayHomeTab')
                 && $this->registerHook('displayHomeTabContent')
             );
@@ -291,7 +267,7 @@ if (!class_exists('BlockUsedProducts')) {
             parent::_clearCache('tab.tpl', 'blockusedproducts-tab');
         }
 
-        public function renderForm()
+        private function renderForm()
         {
             $fields_form = array(
                 'form' => array(
@@ -421,7 +397,7 @@ if (!class_exists('BlockUsedProducts')) {
             return $helper->generateForm(array($fields_form));
         }
 
-        public function getConfigFieldsValues()
+        private function getConfigFieldsValues()
         {
             return array(
                 'PS_NB_DAYS_USED_PRODUCT' => Tools::getValue('PS_NB_DAYS_USED_PRODUCT', Configuration::get('PS_NB_DAYS_USED_PRODUCT')),
